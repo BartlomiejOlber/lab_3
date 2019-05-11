eee3573c17829ddd3a99920322eb393fb57bbb6f
BartlomiejOlber <300237@pw.edu.pl>
2019-05-11 15:16:05 +0200

    first commit

---
+++ src/lab_3.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : lab_3.cpp
+// Author      : d
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
